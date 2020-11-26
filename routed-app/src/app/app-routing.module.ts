8c413d569b6212e858866b423f70ed91664c2e0d
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-11-26 10:54:58 +0000

    tabulazione documenti

--- routed-app/src/app/app-routing.module.ts
+++ routed-app/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = []; //Qui scriveremo le route
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- routed-app/src/app/spotify.service.ts
+++ routed-app/src/app/spotify.service.ts
@@ -3,22 +3,22 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SpotifyService {
-   //url per oauth: https://developer.spotify.com/console/get-search-item/
-  //Ottengo il modulo HttpClient
-  constructor(private http: HttpClient) { }
+    //url per oauth: https://developer.spotify.com/console/get-search-item/
+    //Ottengo il modulo HttpClient
+    constructor(private http: HttpClient) { }
 
-  searchTrack(query: string) {
+    searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQCMpwcm98hgsvTrfvhK4mdIixhBttqPVkBxCtGPGepJumQGv9ieuPOf8R9Oh1peUCnyEwtlE082UWu6WEiUxIAlhsptpe05BuF'
+        Authorization:
+        'Bearer BQBUtyBQfBdcHayWtQrGita0PH0Kd1-eKI4rocFEWSoWVm-HMOD3HUncavkcSnrihz3cLJ3u2R5aFOXUuAK05tR23wIDV_CFEe9zZdzNZvgusxvt3tykdK0nhK5B7XUcxyDPp2MXJWiMQR7xgdwmfiLSFXi48_8'
     });
 
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
- //Ritorno un observable ai componenti che richiedono il servizio
-  }
+    //Ritorno un observable ai componenti che richiedono il servizio
+    }
 }
