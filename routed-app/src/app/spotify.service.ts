c3f6f44bd2f96e4d4f3efb0050ae005f74b0ed8e
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-11-27 21:10:16 +0000

    fixato l'html

--- routed-app/src/app/spotify.service.ts
+++ routed-app/src/app/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
     const headers = new HttpHeaders({
         Authorization:
-        'Bearer BQBUtyBQfBdcHayWtQrGita0PH0Kd1-eKI4rocFEWSoWVm-HMOD3HUncavkcSnrihz3cLJ3u2R5aFOXUuAK05tR23wIDV_CFEe9zZdzNZvgusxvt3tykdK0nhK5B7XUcxyDPp2MXJWiMQR7xgdwmfiLSFXi48_8'
+        'Bearer BQAG_b-_7yC6B65CxAGZmFg2GGsR28P8US7j9BYp0GjsvHiaIEZ5zz48NcGMwa8pTeE7pPSTdF6ss3VjIHLg_9_WJ5ZdLpW05HtO91CeY8ATB05W71p1bgd9vTuwIA3qELiXrPesEeD8sMNvnJLN-F0l3Eu_kk0'
     });
 
     let obsTracks = this.http.get(url, { headers });
